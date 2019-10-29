078fb7085d85489a7a23b4265aa399cb6e32d858
Julia <juliayrkina@gmail.com>
2019-10-29 14:45:56 +0200

    popup modal added

--- NewCMD.js
+++ NewCMD.js
@@ -1,13 +1,11 @@
-$(window).scroll(function () {
+$(window).on('scroll', function () {
     handleTopNavAnimation();
   });
-$(window).load(function () {
+$(window).on('load', function () {
     handleTopNavAnimation();
 });
 
 function handleTopNavAnimation () {
-    $(window).scroll(function () {
-        var $nav = $("#nav-custom");
-            $nav.toggleClass('scrolled', $(this).scrollTop() > 50);
-      });
-  };
+    let $nav = $("#nav-custom");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > 50);
+};
