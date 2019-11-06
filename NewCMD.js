3f34500dda621483d5b32a18d90da7097b93b1a4
Julia <juliayrkina@gmail.com>
2019-11-06 15:46:58 +0200

    "About added"

--- NewCMD.js
+++ NewCMD.js
@@ -1,11 +1,20 @@
 $(window).on('scroll', function () {
     handleTopNavAnimation();
   });
+
 $(window).on('load', function () {
     handleTopNavAnimation();
 });
 
+
 function handleTopNavAnimation () {
-    let $nav = $("#nav-custom");
+    let $nav = $('#nav-custom');
     $nav.toggleClass('scrolled', $(this).scrollTop() > 50);
 };
+
+$(function(){
+    $('.nav-item').on('click', function(){
+        $(this).find('.rotated-angle').toggleClass('rotate');
+    });
+});
+
