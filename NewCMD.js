604ec5ffcfff2831033194c809c300e9f058b755
Julia <juliayrkina@gmail.com>
2019-11-08 13:32:54 +0200

    wip

--- NewCMD.js
+++ NewCMD.js
@@ -9,12 +9,12 @@ $(window).on('load', function () {
 
 function handleTopNavAnimation () {
     let $nav = $('#nav-custom');
-    $nav.toggleClass('scrolled', $(this).scrollTop() > 50);
+    $nav.toggleClass('scrolled', $(this).scrollTop() > 10);
 };
 
-$(function(){
-    $('.nav-item').on('click', function(){
-        $(this).find('.rotated-angle').toggleClass('rotate');
-    });
-});
+// $(function(){
+//     $('.nav-item').on('click', function(){
+//         $(this).find('.rotated-angle').toggleClass('rotate');
+//     });
+// });
 
